6860613edd3bd4856d5d861aed77f8f3d5c2b9d4
Nur99 <nayapbergen@gmail.com>
2018-06-25 11:26:54 +0600

    Catalan.cpp

---
+++ DP/Basic/Catalan.cpp
@@ -0,0 +1,10 @@
+/*
+First: How many there are ways to get from (0,0) to (n,n) without crossing main diagonal.
+
+Second: int n = 4, how many ways we have to construct right parenthesis structrue with length n.
+if n = 4 output: 2
+(()), ()()
+
+Answer must be catalan numbers!
+Nth Catalan number = C(n, 2*n)/(n+1);
+*/
