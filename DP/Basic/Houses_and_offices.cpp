91ac03210f33961d70015e3d2b49d4248e8046cc
Nur99 <nayapbergen@gmail.com>
2018-06-28 07:45:29 +0600

    Count ways to build street under given constraints

---
+++ DP/Basic/Houses_and_offices.cpp
@@ -0,0 +1,16 @@
+/*
+Just know how to go from one condition to another. It is enough.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int n, dp[101][3];
+int main(){
+    cin>>n;
+    dp[1][0] = dp[1][1] = dp[1][2] = 1;
+    for(int i = 2; i <= n; i++){
+        dp[i][0] += dp[i-1][0] + dp[i-1][1] + dp[i-1][2];
+        dp[i][1] += dp[i-1][0] + dp[i-1][2];
+        dp[i][2] += dp[i-1][0] + dp[i-1][1];
+    }
+    cout<<dp[n][0]+dp[n][1]+dp[n][2];
+}
