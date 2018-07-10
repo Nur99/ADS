49414fbc4735d4cbb8b61ac8c401ad0fa0f5ae95
Nur99 <nayapbergen@gmail.com>
2018-07-10 16:10:42 +0600

    Update dot_product.cpp

--- DP/INTERMEDIATE/dot_product.cpp
+++ DP/INTERMEDIATE/dot_product.cpp
@@ -1,9 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long int MaxDotProduct(int A[], int B[],
-                            int m, int n)
-{
-    long long int dp[n+1][m+1];
+#define int long long 
+int MaxDotProduct(int A[], int B[], int m, int n){
+    int dp[n+1][m+1];
     memset(dp, 0, sizeof(dp));
  
     for (int i=1; i<=n; i++)
@@ -11,16 +10,16 @@ long long int MaxDotProduct(int A[], int B[],
             dp[i][j] = max((dp[i-1][j-1] + (A[j-1]*B[i-1])), dp[i][j-1]);
     return dp[n][m] ;
 }
-int main()
+int32_t main()
 {
   int t, m, n;
   cin>>t;
   
   while(t--){
     cin>>m>>n;
-    int A[n], B[m];
-    for(int i = 1; i <= m; i++)cin>>A[i];
-    for(int j = 1; j <= n; j++)cin>>B[j];
+    int A[m], B[n];
+    for(int i = 0; i < m; i++)cin>>A[i];
+    for(int j = 0; j < n; j++)cin>>B[j];
     cout << MaxDotProduct(A, B, m, n)<<endl;
   }
 }
