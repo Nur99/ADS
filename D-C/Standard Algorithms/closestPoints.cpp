7bf268dd3cad2b631b5c701b68f9f24601dc4476
Nur99 <nayapbergen@gmail.com>
2018-08-22 20:07:24 +0600

    Update closestPoints.cpp

--- D-C/Standard Algorithms/closestPoints.cpp
+++ D-C/Standard Algorithms/closestPoints.cpp
@@ -1,18 +1,16 @@
-// A divide and conquer program in C++ to find the smallest distance from a
+// A divide and conquer program in C/C++ to find the smallest distance from a
 // given set of points.
  
-#include <iostream>
+#include <stdio.h>
 #include <float.h>
 #include <stdlib.h>
 #include <math.h>
-using namespace std;
  
 // A structure to represent a Point in 2D plane
 struct Point
 {
     int x, y;
 };
- 
  
 /* Following two functions are needed for library function qsort().
    Refer: http://www.cplusplus.com/reference/clibrary/cstdlib/qsort/ */
@@ -66,6 +64,8 @@ float stripClosest(Point strip[], int size, float d)
 {
     float min = d;  // Initialize the minimum distance as d
  
+    qsort(strip, size, sizeof(Point), compareY); 
+ 
     // Pick all points one by one and try the next points till the difference
     // between y coordinates is smaller than d.
     // This is a proven fact that this loop runs at most 6 times
@@ -77,38 +77,23 @@ float stripClosest(Point strip[], int size, float d)
     return min;
 }
  
-// A recursive function to find the smallest distance. The array Px contains
-// all points sorted according to x coordinates and Py contains all points
-// sorted according to y coordinates
-float closestUtil(Point Px[], Point Py[], int n)
+// A recursive function to find the smallest distance. The array P contains
+// all points sorted according to x coordinate
+float closestUtil(Point P[], int n)
 {
     // If there are 2 or 3 points, then use brute force
     if (n <= 3)
-        return bruteForce(Px, n);
+        return bruteForce(P, n);
  
     // Find the middle point
     int mid = n/2;
-    Point midPoint = Px[mid];
- 
- 
-    // Divide points in y sorted array around the vertical line.
-    // Assumption: All x coordinates are distinct.
-    Point Pyl[mid+1];   // y sorted points on left of vertical line
-    Point Pyr[n-mid-1];  // y sorted points on right of vertical line
-    int li = 0, ri = 0;  // indexes of left and right subarrays
-    for (int i = 0; i < n; i++)
-    {
-      if (Py[i].x <= midPoint.x)
-         Pyl[li++] = Py[i];
-      else
-         Pyr[ri++] = Py[i];
-    }
+    Point midPoint = P[mid];
  
     // Consider the vertical line passing through the middle point
     // calculate the smallest distance dl on left of middle point and
     // dr on right side
-    float dl = closestUtil(Px, Pyl, mid);
-    float dr = closestUtil(Px + mid, Pyr, n-mid);
+    float dl = closestUtil(P, mid);
+    float dr = closestUtil(P + mid, n-mid);
  
     // Find the smaller of two distances
     float d = min(dl, dr);
@@ -118,8 +103,8 @@ float closestUtil(Point Px[], Point Py[], int n)
     Point strip[n];
     int j = 0;
     for (int i = 0; i < n; i++)
-        if (abs(Py[i].x - midPoint.x) < d)
-            strip[j] = Py[i], j++;
+        if (abs(P[i].x - midPoint.x) < d)
+            strip[j] = P[i], j++;
  
     // Find the closest points in strip.  Return the minimum of d and closest
     // distance is strip[]
@@ -130,19 +115,10 @@ float closestUtil(Point Px[], Point Py[], int n)
 // This method mainly uses closestUtil()
 float closest(Point P[], int n)
 {
-    Point Px[n];
-    Point Py[n];
-    for (int i = 0; i < n; i++)
-    {
-        Px[i] = P[i];
-        Py[i] = P[i];
-    }
- 
-    qsort(Px, n, sizeof(Point), compareX);
-    qsort(Py, n, sizeof(Point), compareY);
+    qsort(P, n, sizeof(Point), compareX);
  
     // Use recursive function closestUtil() to find the smallest distance
-    return closestUtil(Px, Py, n);
+    return closestUtil(P, n);
 }
  
 // Driver program to test above functions
@@ -150,6 +126,6 @@ int main()
 {
     Point P[] = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};
     int n = sizeof(P) / sizeof(P[0]);
-    cout << "The smallest distance is " << closest(P, n);
+    printf("The smallest distance is %f ", closest(P, n));
     return 0;
 }
