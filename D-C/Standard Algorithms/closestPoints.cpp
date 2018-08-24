e4e1c23a8b6a5168b404810144f382bc9bc38ab8
Nur99 <nayapbergen@gmail.com>
2018-08-24 16:53:33 +0600

    Update closestPoints.cpp

--- D-C/Standard Algorithms/closestPoints.cpp
+++ D-C/Standard Algorithms/closestPoints.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define pb push_back
 #define mp make_pair
 
-pair <int, int> p[100001];
+pair <int, int> px[100001], py[100001];
 float dist(int i, int j){
 	return sqrt(abs(p[i].first - p[j].second) + abs(p[i].first - p[j].second)); 	
 }
@@ -19,10 +19,6 @@ float brute(int l, int r){
 	return ans;
 }
 float stripUtil(pair <int, int> strip[], int n, float d){
-	for(int i = 1; i < n; i++){
-	   strip[i] = mp(strip[i].second, strip[i].first);
-	}
-	sort(strip, strip+n+1);
 	for(int i = 1; i <= n; i++){
 	   for(int j = i+1; j <= n; j++){
 	    	if(p[j].first - p[i].first > d)break;
@@ -31,29 +27,37 @@ float stripUtil(pair <int, int> strip[], int n, float d){
 	}
 	return d;
 }
-float closestUtil(int l, int r){
+float closestUtil(int l, int r, pair <int, int> py[]){
 	if(r-l+1 <= 3) return brute(l, r); 	
 
 	int m = (l+r)/2;
-
-	float dl = closestUtil(l, m);
-	float dr = closestUtil(m+1, r);
+	int n = r-l+1;
+	
+	pair <int, int> pyl[n], pyr[n];
+	
+	int pi = 0, pj = 0;
+	for(int i = l; i <= r; i++){
+		if(p[i].x <= p[m].x)pyl[pi] = py[i], pi++;
+		else pyr[pj] = py[i], pj++;
+	}	
+	
+	float dl = closestUtil(l, m, pyl);
+	float dr = closestUtil(m+1, r, pyr);
 	float d = min(dl, dr);
 
 
-	int n = r-l+1;
 	pair <int, int> strip[n+1];
 	int j = 1;
 
 	for(int i = l; i <= r; i++){
-		if(abs(p[i].first - p[m].first) < d)strip[j] = p[i], j++; 	
+		if(abs(py[i].first - p[m].first) < d)strip[j] = py[i], j++; 	
 	}
 	return min(d, stripUtil(strip, j, d));
 }
 float closest(int n){
- 	sort(p, p+n+1);
- 	
- 	return closestUtil(1, n);
+ 	sort(px, px+n+1);
+	sort(py, py+n+1);
+ 	return closestUtil(1, n, px, py);
 }
 int main(){
 	int n;
@@ -62,8 +66,8 @@ int main(){
  	for(int i = 1; i <= n; i++){
  		int x, y;
  		cin>>x>>y;
- 		p[i] = mp(x, y);
- 	}
-
+ 		px[i] = mp(x, y);
+ 		py[i] = mp(y, x);
+	}
  	cout<<closest(n);
 }
