d57a59891e112125672923146e232cfb13b5644b
Nur99 <nayapbergen@gmail.com>
2018-08-31 19:10:13 +0600

    Create fibo_encoding.cpp

---
+++ Math/Fibonacci/fibo_encoding.cpp
@@ -0,0 +1,9 @@
+According to Zeckendorf's theorem, every positive number can be sum of some of fibonacci numbers. But the fibonacci numbers are not 
+neighbours of each other. 
+
+
+How to encode?
+
+https://www.geeksforgeeks.org/fibonacci-coding/
+
+don't watch to code, watch to image and you will understand. 
