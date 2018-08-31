48b768daa342f276a61d81ed8d06cd6e79532e00
Nur99 <nayapbergen@gmail.com>
2018-08-31 19:37:01 +0600

    Create fibo_periodic_under_modulo.cpp

---
+++ Math/Fibonacci/fibo_periodic_under_modulo.cpp
@@ -0,0 +1,5 @@
+Task: Find n'th multiple (kratnoe) of k in Fibonacci series. 
+
+
+If we take fibonacci sequence under modulo of "k" we notice that 0 is repeating in a constant interval. 
+We should find formula and return it.
