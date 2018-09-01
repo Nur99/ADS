dc31df8a5e6a5fd791d13619b80ca7ee23abf5ff
Nur99 <nayapbergen@gmail.com>
2018-09-01 15:38:26 +0600

    Update fibo_periodic_under_modulo.cpp

--- Math/Fibonacci/fibo_periodic_under_modulo.cpp
+++ Math/Fibonacci/fibo_periodic_under_modulo.cpp
@@ -1,5 +1,21 @@
-Task: Find n'th multiple (kratnoe) of k in Fibonacci series. 
+#include <bits/stdc++.h>
+using namespace std;
 
-
-If we take fibonacci sequence under modulo of "k" we notice that 0 is repeating in a constant interval. 
-We should find formula and return it.
+#define int long long
+int32_t main(){
+    int n, m;
+    
+    cin>>n>>m;
+    
+    //n'th multiple of m
+    
+    int a = 0, b = 1, c = 1;
+    for(int i = 3; i <= 1000; i++){
+        c = a + b;
+        a = b;
+        b = c;
+        if(c % m == 0)cout<<n*i, exit(0);
+    }
+    
+    
+}
