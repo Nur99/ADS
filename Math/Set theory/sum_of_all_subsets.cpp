ec7a23883893e0d45141d9e9a451ae6299df0451
Nurymzhan <nayapbergen@gmail.com>
2018-09-15 15:26:59 +0600

    Update sum_of_all_subsets.cpp

--- Math/Set theory/sum_of_all_subsets.cpp
+++ Math/Set theory/sum_of_all_subsets.cpp
@@ -1,14 +1,9 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-long long findSumSubsets(int n) 
-{ 
-    return (n * (n + 1) / 2) * (1 << (n - 1)); 
-} 
-  
-int main() 
-{ 
-    int n = 3; 
-    cout << findSumSubsets(n); 
-    return 0; 
-} 
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    cout<<"Sum of all subsets: ";
+    cout<<n*(n+1)/2*(1 << (n-1));
+}
