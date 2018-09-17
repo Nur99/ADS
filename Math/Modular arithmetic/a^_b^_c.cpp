58ceb67f5369085a18d7862351e633a0b43810a7
Nurymzhan <nayapbergen@gmail.com>
2018-09-17 18:21:04 +0600

    Update a^_b^_c.cpp

--- Math/Modular arithmetic/a^_b^_c.cpp
+++ Math/Modular arithmetic/a^_b^_c.cpp
@@ -1,19 +1,42 @@
-# Python program to calculate the ans
-def calculate(A, B, C, M):
- 
-    # Calculate B ^ C (mod M - 1)
-    res = pow(B, C, M-1)
- 
-    # Calculate A ^ res ( mod M )
-    ans = pow(A, res, M)
- 
-    return ans
- 
-# Driver program to run the case
-A = 3
-B = 9
-C = 4
- 
-# M must be Prime Number
-M = 19
-print( calculate(A, B, C, M) )
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+some theorem about code
+
+A^B^C % M
+
+res = B^C
+
+A^(p-1) = 1
+
+B^C = X*(M-1) + Y;
+
+Y = B^C % (M-1)
+
+A^(B^C % M-1) % M 
+
+*/
+int pow(int a, int b, int m){
+    if(b == 0)return 1;
+    if(b == 1)return a%m;
+    
+    int half = pow(a, b/2, m);
+    
+    if(b%2 == 1)(half*a)%m;
+    
+    return (half*half)%m;
+}
+int find(int a, int b, int c){
+    int res = pow(b, c, m-1);
+    
+    int ans = pow(a, res, m);
+    cout<<ans;
+}
+int main(){
+    int a, b, c, m;
+    cin>>a>>b>>c>>m;
+    
+    find(a, b, c, m);
+    
+}
