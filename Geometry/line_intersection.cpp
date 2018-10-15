d3c10eac28907d6fdec4532e8ac54169fbca19ec
Nurymzhan <nayapbergen@gmail.com>
2018-10-15 14:26:46 +0600

    Update line_intersection.cpp

--- Geometry/line_intersection.cpp
+++ Geometry/line_intersection.cpp
@@ -3,20 +3,20 @@ using namespace std;
 struct point{
     int x, y;
 };
+
 struct line{
-    point p1, p2;  
+    point p1, p2;
 };
 
 int orientation(point a, point b, point c){
-    //int v = (x3-x2)*(y2-y) - (x2-x)*(y3-y2);
     int v = (c.x-b.x)*(b.y-a.y) - (b.x-a.x)*(c.y-b.y);
-    
+
     if(!v)return 0;
     return (v > 0) ?1 :2;
 }
 
 bool onSegment(point a, point b, point c){
-    if(a.x <= b.x && b.x <= c.x && a.y <= b.y && b.y <= c.y)return 1;
+    if(min(a.x, c.x) <= b.x && b.x <= max(a.x, c.x) && min(a.y, c.y) <= b.y && b.y <= max(a.y, c.y))return 1;
     return 0;
 }
 
@@ -30,31 +30,30 @@ bool intersect(line l, line l2){
     if(o1 != o2 && o3 != o4)return 1;
     
     if(!o1 && onSegment(l.p1, l2.p1, l.p2))return 1;
-    if(!o1 && onSegment(l.p1, l2.p2, l.p2))return 1;
-    if(!o1 && onSegment(l2.p1, l.p1, l2.p2))return 1;
-    if(!o1 && onSegment(l2.p1, l.p2, l2.p2))return 1;    
+    if(!o2 && onSegment(l.p1, l2.p2, l.p2))return 1;
+    if(!o3 && onSegment(l2.p1, l.p1, l2.p2))return 1;
+    if(!o4 && onSegment(l2.p1, l.p2, l2.p2))return 1;
     
     return 0;
 }
 
 int main(){
     
-    point a, b;
-    cin>>a.x>>a.y>>b.x>>b.y;
+    int t;
+    cin>>t;
     
-    line l;
-    l.p1 = a, l.p2 = b;
-    
-    point c, d;
-    cin>>c.x>>c.y>>d.x>>d.y;
-    
-    line l2;
-    l2.p1 = c, l2.p2 = d;
-    
-    
-    if(intersect(l, l2))cout<<"Yes";
-    else cout<<"No";
-    
-    
+    while(t--){
+        point a, b;
+        cin>>a.x>>a.y>>b.x>>b.y;
+        line l;
+        l.p1 = a, l.p2 = b;
+        point c, d;
+        cin>>c.x>>c.y>>d.x>>d.y;
+        line l2;
+        l2.p1 = c, l2.p2 = d;
+        if(intersect(l, l2))cout<<1;
+        else cout<<0;
+        cout<<endl;
+    }
     
 }
