3934f110ee021fc9ffcdbddf41205cfa6aeb3ff9
Nurymzhan <nayapbergen@gmail.com>
2018-10-19 03:24:32 +0600

    Create points_in_triangle.cpp

---
+++ Geometry/points_in_triangle.cpp
@@ -0,0 +1,2 @@
+For example, if we have point P, we should calculate the areas of PAB, PBC, PAC. Then add them together, 
+if thiese areas' sum is equal to area of ABC, then the point P lies on ABC.
